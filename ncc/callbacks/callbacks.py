499959af6317739091f235fc848555496ad95824
hiroki <1234defgsigeru@gmail.com>
2019-08-05 10:33:25 +0900

    update callback base

--- ncc/callbacks/callbacks.py
+++ ncc/callbacks/callbacks.py
@@ -75,10 +75,10 @@ class Callback(object):
     def on_batch_begin(self, batch, logs=None):
         pass
 
-    def on_train_batch_begin(self, batch, logs=None):
+    def on_batch_end(self, batch, logs=None):
         pass
 
-    def on_batch_end(self, batch, logs=None):
+    def on_train_batch_begin(self, batch, logs=None):
         pass
 
     def on_train_batch_end(self, batch, logs=None):
@@ -90,6 +90,29 @@ class Callback(object):
     def on_train_end(self, logs=None):
         pass
 
+    def on_test_batch_begin(self, batch, logs=None):
+        pass
+
+    def on_test_batch_end(self, batch, logs=None):
+        pass
+
+    def on_test_begin(self, logs=None):
+        pass
+
+    def on_test_end(self, logs=None):
+        pass
+
+    def on_predict_batch_begin(self, batch, logs=None):
+        pass
+
+    def on_predict_batch_end(self, batch, logs=None):
+        pass
+
+    def on_predict_begin(self, logs=None):
+        pass
+
+    def on_predict_end(self, logs=None):
+        pass
 
 # ==========================================================================
 #

6bb35f19e8d404fe718a7fc94547a8c6e1a9f2ac
aiorhiroki <1234defgsigeru@gmail.com>
2019-08-23 21:18:44 +0900

    image util

--- ncc/utils/image.py
+++ ncc/utils/image.py
@@ -45,3 +45,57 @@ def change_color_palettes(image_files, colors):
             numpy_image[numpy_image == color] = i
         pil_palette = convert_to_palette(numpy_image)
         pil_palette.save(image_file)
+
+
+def concat_images(im1, im2, palette, mode):
+    if mode == "P":
+        assert palette is not None
+        dst = Image.new("P", (im1.width + im2.width, im1.height))
+        dst.paste(im1, (0, 0))
+        dst.paste(im2, (im1.width, 0))
+        dst.putpalette(palette)
+    elif mode == "RGB":
+        dst = Image.new("RGB", (im1.width + im2.width, im1.height))
+        dst.paste(im1, (0, 0))
+        dst.paste(im2, (im1.width, 0))
+    else:
+        raise NotImplementedError
+
+    return dst
+
+
+def cast_to_pil(ndarray, palette, index_void=None):
+    # index_void: 境界線のindexで学習・可視化の際は背景色と同じにする。
+    assert len(ndarray.shape) == 3
+    res = np.argmax(ndarray, axis=2)
+    if index_void is not None:
+        res = np.where(res == index_void, 0, res)
+    image = Image.fromarray(np.uint8(res), mode="P")
+    image.putpalette(palette)
+    return image
+
+
+def get_imageset(
+    image_in_np,
+    image_out_np,
+    image_gt_np,
+    palette,
+    index_void=None
+):
+    # 3つの画像(in, out, gt)をくっつけます。
+    image_out = cast_to_pil(
+        image_out_np, palette, index_void
+    )
+    image_tc = cast_to_pil(
+        image_gt_np, palette, index_void
+    )
+    image_merged = concat_images(
+        image_out, image_tc, palette, "P"
+    ).convert("RGB")
+    image_in_pil = Image.fromarray(
+        np.uint8(image_in_np * 255), mode="RGB"
+    )
+    image_result = concat_images(
+        image_in_pil, image_merged, None, "RGB"
+    )
+    return image_result

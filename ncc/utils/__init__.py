0755b437e402cbacbbecf2de3cb2d50cc546a034
aiorhiroki <1234defgsigeru@gmail.com>
2019-08-23 21:11:24 +0900

    slack logging

--- ncc/utils/__init__.py
+++ ncc/utils/__init__.py
@@ -2,3 +2,4 @@ from .image import *
 from .palette import palettes
 from .move_file import separate_folder, move_folder
 from .plot import MatPlot, MatPlotManager
+from .logging import slack_logging

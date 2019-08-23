e5a4afeb3cf7765c5c65cd41c7ed0f54d5dc0a4f
aiorhiroki <1234defgsigeru@gmail.com>
2019-08-23 21:54:53 +0900

    image util

--- ncc/utils/image.py
+++ ncc/utils/image.py
@@ -3,6 +3,26 @@ from .palette import palettes
 import numpy as np
 import random
 from PIL import Image
+import cv2
+from albumentations import (
+    PadIfNeeded,
+    HorizontalFlip,
+    VerticalFlip,
+    CenterCrop,
+    Crop,
+    Compose,
+    Transpose,
+    RandomRotate90,
+    ElasticTransform,
+    GridDistortion,
+    OpticalDistortion,
+    RandomSizedCrop,
+    OneOf,
+    CLAHE,
+    RandomBrightnessContrast,
+    RandomGamma,
+    Normalize
+)
 
 
 def random_colors(N, bright=True, scale=True, shuffle=False):
@@ -99,3 +119,123 @@ def get_imageset(
         image_in_pil, image_merged, None, "RGB"
     )
     return image_result
+
+
+def generate_sample_result(
+    model,
+    annotations,
+    nb_classes,
+    height,
+    width
+):
+    image_util = ImageUtil(nb_classes, (height, width))
+    file_length = len(annotations)
+    random_index = np.random.randint(file_length)
+    sample_image_path = annotations[random_index]
+    sample_image = image_util.read_image(
+        sample_image_path[0],
+        anti_alias=True
+    )
+    segmented = image_util.read_image(
+        sample_image_path[1],
+        normalization=False
+    )
+    sample_image = np.asarray(sample_image, dtype=np.float32)
+    segmented = np.asarray(segmented, dtype=np.uint8)
+    segmented = image_util.cast_to_onehot(segmented)
+    output = model.predict(np.expand_dims(sample_image, axis=0))
+
+    return [sample_image, output[0], segmented]
+
+
+class ImageUtil:
+
+    def __init__(
+        self,
+        nb_classes: int,
+        size: (int, int)
+    ):
+        self.nb_classes = nb_classes
+        self.size = size[::-1]
+        self.current_raw_size = None
+        self.current_raw_frame = None
+
+    def read_image(
+        self,
+        file_path: str,
+        normalization=True,
+        anti_alias=False
+    ):
+        image = Image.open(file_path)
+        self.current_raw_frame = image
+        self.current_raw_size = image.size
+        if self.size != self.current_raw_size:
+            resample = Image.LANCZOS if anti_alias else Image.NEAREST
+            image = image.resize(self.size, resample)
+        # delete alpha channel
+        if image.mode == "RGBA":
+            image = image.convert("RGB")
+        image = np.asarray(image)
+        if normalization:
+            image = image / 255.0
+
+        return image
+
+    def cast_to_onehot(
+        self,
+        labels: list
+    ):
+        labels = np.asarray(labels, dtype=np.uint8)
+        # Classification
+        if len(labels.shape) == 1:
+            one_hot = np.eye(self.nb_classes, dtype=np.uint8)
+        # Segmentation
+        else:
+            one_hot = np.identity(self.nb_classes, dtype=np.uint8)
+        return one_hot[labels]
+
+    def _cast_to_frame(
+        self,
+        prediction,
+        size
+    ):
+        res = np.argmax(prediction, axis=2)
+        image = Image.fromarray(np.uint8(res), mode="P")
+        image.putpalette(palettes)
+        image = image.resize(self.current_raw_size, Image.LANCZOS)
+        image = image.convert("RGB")
+        return np.asarray(np.asarray(image)*255, dtype=np.uint8)
+
+    def blend_image(
+        self,
+        output_image,
+        size
+    ):
+        input_frame = np.array(self.current_raw_frame, dtype=np.uint8)
+        output_frame = self._cast_to_frame(output_image, size)
+        blended = cv2.addWeighted(
+            src1=input_frame,
+            src2=output_frame,
+            alpha=0.7,
+            beta=0.9,
+            gamma=2.2
+        )
+        return cv2.cvtColor(blended, cv2.COLOR_RGB2BGR)
+
+    def augmentation(self, image, mask):
+        width, height = self.size
+        aug_list = [
+            RandomSizedCrop(
+                min_max_height=(height*3//4, height),
+                height=height,
+                width=width,
+                p=0.5
+            ),
+            HorizontalFlip(
+                p=0.5
+            )
+        ]
+
+        aug = Compose(aug_list, p=1)
+        augmented = aug(image=image, mask=mask)
+        return augmented['image'], augmented['mask']

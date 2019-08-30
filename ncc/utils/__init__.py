200916a90da0eb2b8e84e5544b138cb55bafc9ba
aiorhiroki <1234defgsigeru@gmail.com>
2019-08-31 00:04:25 +0900

    set keras env

--- ncc/utils/__init__.py
+++ ncc/utils/__init__.py
@@ -3,3 +3,4 @@ from .palette import palettes
 from .move_file import separate_folder, move_folder
 from .plot import MatPlot, MatPlotManager
 from .post_client import PostClient
+from .set_env import set_keras_env

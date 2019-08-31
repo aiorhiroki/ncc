f28895a778e83affc20c3cc4ac5e2f99888b35b5
aiorhiroki <1234defgsigeru@gmail.com>
2019-08-31 17:35:16 +0900

    direct import

--- ncc/__init__.py
+++ ncc/__init__.py
@@ -1,3 +1,13 @@
 from .models import *
+from .utils import *
+from .readers import *
+from .callbacks import *
+from .metrics import *
+from .generators import *
+from .augmentation import *
+from .dataset import *
+from .validations import *
+from .predictions import *
+from .preprocessing import *
 
 __version__ = '0.0.1'

2f538414bc128c84461d9aaf563c560cd454cdf6
aiorhiroki <1234defgsigeru@gmail.com>
2019-09-22 02:33:15 +0900

    pytorch

--- ncc/models/mobilenet.py
+++ ncc/models/mobilenet.py
@@ -3,17 +3,25 @@ from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
 from tensorflow.keras.applications.mobilenet import MobileNet
 from tensorflow.keras.models import Model
 
+import pretrainedmodels
 
-def mobilenet(nb_classes, width=224, height=224):
-    with tf.device("/cpu:0"):
-        base_model = MobileNet(
-            input_shape=(width, height, 3),
-            weights='imagenet',
-            include_top=False
+
+def mobilenet(nb_classes, width=244, height=244, framework="tensorflow"):
+    if framework == "tensorflow":
+        with tf.device("/cpu:0"):
+            base_model = MobileNet(
+                input_shape=(width, height, 3),
+                weights='imagenet',
+                include_top=False
+            )
+            x = base_model.output
+            x = GlobalAveragePooling2D()(x)
+            predictions = Dense(nb_classes, activation='softmax')(x)
+            model = Model(base_model.input, predictions)
+
+    elif framework == "pytorch":
+        model = pretrainedmodels.__dict__['mobilenet'](
+            num_classes=nb_classes, pretrained='imagenet'
         )
-        x = base_model.output
-        x = GlobalAveragePooling2D()(x)
-        predictions = Dense(nb_classes, activation='softmax')(x)
-        model = Model(base_model.input, predictions)
 
     return model
--- ncc/models/xception.py
+++ ncc/models/xception.py
@@ -3,17 +3,25 @@ from keras.layers import Dense, GlobalAveragePooling2D
 from keras.applications.xception import Xception
 from keras.models import Model
 
+import pretrainedmodels
 
-def xception(nb_classes, width=299, height=299):
-    with tf.device("/cpu:0"):
-        base_model = Xception(
-            input_shape=(width, height, 3),
-            weights='imagenet',
-            include_top=False
+
+def xception(nb_classes, width=299, height=299, framework="tensorflow"):
+    if framework == "tensorflow":
+        with tf.device("/cpu:0"):
+            base_model = Xception(
+                input_shape=(width, height, 3),
+                weights='imagenet',
+                include_top=False
+            )
+            x = base_model.output
+            x = GlobalAveragePooling2D()(x)
+            predictions = Dense(nb_classes, activation='softmax')(x)
+            model = Model(base_model.input, predictions)
+
+    elif framework == "pytorch":
+        model = pretrainedmodels.__dict__['xception'](
+            num_classes=nb_classes, pretrained='imagenet'
         )
-        x = base_model.output
-        x = GlobalAveragePooling2D()(x)
-        predictions = Dense(nb_classes, activation='softmax')(x)
-        model = Model(base_model.input, predictions)
 
     return model

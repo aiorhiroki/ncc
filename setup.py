25953176c9f99ea751e083dc73b9d08deb02c935
hiroki <1234defgsigeru@gmail.com>
2019-12-20 10:49:44 +0900

    v1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='ncc',
-    version='0.1.0',
+    version='1.0.0',
     description='NCC AI Tools',
     author='Hiroki Matsuzaki',
     author_email='1234defgsigeru@gmail.com',

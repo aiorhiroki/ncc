67c1fba060382b3ed17ec5a877e436fca27af054
aiorhiroki <1234defgsigeru@gmail.com>
2019-12-26 23:07:26 +0900

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='ncc',
-    version='1.0.0',
+    version='1.0.1',
     description='NCC AI Tools',
     author='Hiroki Matsuzaki',
     author_email='1234defgsigeru@gmail.com',
